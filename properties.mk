20d7fb8ac2afc87b9c6e4b6882b2fc3e8eb7f91b
causeri3 <vanessaca@web.de>
2026-01-25 11:13:52 +0100

    change device

--- properties.mk
+++ properties.mk
@@ -1,4 +1,4 @@
-DEVICE = instinctcrossoveramoled
+DEVICE = descentmk2s
 SDK_HOME = ~/Library/Application\ Support/Garmin/ConnectIQ/Sdks/connectiq-sdk-mac-8.4.0-2025-12-03-5122605dc
 PRIVATE_KEY = /Users/your_username/garmin_developer_key
 APP_ID = 46e9c768-4eb1-470c-93a8-29dd11219b61
